2b7bf1576b49d3794f1f362ec5084d94902f5141
David Eccles (gringer) <github@gringer.org>
2014-02-07 20:12:44 +1300

    Reheader to new license text that is the same as GNU recommendations

--- include/dtree.hpp
+++ include/dtree.hpp
@@ -1,22 +1,23 @@
 /** <header>
+
+ * This file is part of preBowt -- a prefix-based BWT-like transform of
+ * genetic data.
  *
  * Copyright 2014 David Eccles (gringer) <bioinformatics@gringene.org>
  *
- * This file is part of preBowt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * preBowt is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * preBowt is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with preBowt. If not, see <http://www.gnu.org/licenses/>
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 </header> **/
 
 #ifndef __DTREE_HPP__
